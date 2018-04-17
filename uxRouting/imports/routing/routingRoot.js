96caa73c9cd8f29703fef75d2b5acd1f3899282e
Paweł Cesar Sanjuan Szklarz <p@eiipii.com>
2018-04-17 22:57:35 +0200

    layouts

--- uxRouting/imports/routing/routingRoot.js
+++ uxRouting/imports/routing/routingRoot.js
@@ -6,7 +6,9 @@ import bootstrapRootTemplate from './views/bootstrapRootTemplate.html';
 import column12Flow from './views/boot/colomn12Flow.html';
 import column6x6Flow from './views/boot/column6x6Flow.html';
 import column10x2Flow from './views/boot/column10x2Flow.html';
-import hccfLayout from './views/boot/hccfLayout.html';
+import hccfLayout from './views/layout/hccfLayout.html';
+import hhccfLayout from './views/layout/hhccfLayout.html';
+import navContentLayout from './views/layout/navContentLayout.html';
 
 import mediaContent from './views/boot/mediaContent.html';
 import wellContent from './views/boot/wellContent.html';
@@ -47,6 +49,10 @@ var appListOfStates = [{
 }, {
     name: 'routing',
     url: "/routing",
+    templateUrl: routingTemplate
+}, {
+    name: 'layouts',
+    url: "/layouts",
     templateUrl: routingTemplate
 }, {
     name: 'routing.bootstrapContainer',
@@ -121,11 +127,11 @@ var appListOfStates = [{
         }
     }
 }, {
-    name: 'routing.bootstrapContainer.hccfLayout',
+    name: 'layouts.hccfLayout',
     url: "/hccf",
     templateUrl: hccfLayout
 }, {
-    name: 'routing.bootstrapContainer.hccfLayout.standard',
+    name: 'layouts.hccfLayout.standard',
     url: "/standard",
     views: {
         header: {
@@ -139,6 +145,93 @@ var appListOfStates = [{
         },
         footer: {
             templateUrl: wellContent
+        }
+    }
+}, {
+    name: 'layouts.hccfLayout.color',
+    url: "/color",
+    views: {
+        header: {
+            template: '<img holder="holder.js/100px200?bg=#162955&fg=#7887AB">'
+        },
+        leftCol: {
+            template: '<img holder="holder.js/100px200?bg=#c3f3ab&fg=#9cc288">'
+        },
+        rightCol: {
+            template: '<img holder="holder.js/100px200?bg=#675E4C&fg=#4A5A42">'
+        },
+        footer: {
+            template: '<img holder="holder.js/100px200?bg=#9D344B&fg=#EC9DAE">'
+        }
+    }
+}, {
+    name: 'layouts.navContent',
+    url: "/navContent",
+    templateUrl: navContentLayout
+}, {
+    name: 'layouts.navContent.color',
+    url: "/color",
+    views: {
+        nav : {
+            template: '<img holder="holder.js/100px400?theme=social&text=Navigation">'
+        },
+        content : {
+            template: '<img holder="holder.js/100px800?theme=social&text=Content">'
+        }
+    }
+}, {
+    name: 'layouts.navContent.content',
+    url: "/content",
+    views: {
+        nav : {
+            templateUrl: navStackedContent
+        },
+        content : {
+            templateUrl: mediaContent
+        }
+    }
+}, {
+    name: 'layouts.hhccfLayout',
+    url: "/hhccf",
+    templateUrl: hhccfLayout
+}, {
+    name: 'layouts.hhccfLayout.content',
+    url: "/content",
+    views: {
+        menu: {
+            templateUrl: navigationContent
+        },
+        header: {
+            template: '<img holder="holder.js/100px100?theme=social&text=Header">'
+        },
+        leftCol: {
+            templateUrl: mediaContent
+        },
+        rightCol: {
+            templateUrl: navStackedContent
+        },
+        footer: {
+            templateUrl: wellContent
+        }
+    }
+}, {
+    name: 'layouts.hhccfLayout.color',
+    url: "/color",
+    views: {
+        menu: {
+            template: '<img holder="holder.js/100px120?theme=social&text=Menu">'
+        },
+        header: {
+            template: '<img holder="holder.js/100px120?theme=sky&text=Header">'
+        },
+        leftCol: {
+            template: '<img holder="holder.js/100px500?theme=gray&text=LeftCol">'
+        },
+        rightCol: {
+            template: '<img holder="holder.js/100px500?bg=#675E4C&fg=#4A5A42">'
+        },
+        footer: {
+            template: '<img holder="holder.js/100px200?bg=#9D344B&fg=#EC9DAE">'
         }
     }
 }, {
